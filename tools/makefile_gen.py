8f5f85bd7eb12db88454323c85f60a96af2aba3d
Manni Moghimi <mannimoghimi@gmail.com>
2024-09-01 11:43:18 +0330

    feat: modified Makefile and makefile_generator to create new make file with ecmus-qos-aware plots

--- tools/makefile_gen.py
+++ tools/makefile_gen.py
@@ -1,7 +1,15 @@
 FILE_PATH = "Makefile.iter"
 
 KIND = "normal_multiple"
-SCRIPT_NAME = "placement_ratio_linechart"
+
+SCRIPT_NAMES = [
+    "average_latency_boxplot",
+    "average_latency_linechart",
+    "edge_utilization_linechart",
+    "placement_ratio_linechart",
+    "pod_count_linechart",
+]
+
 REPORT_FILE_COUNT = 10
 
 REPORT_DIR_NAMES = [
@@ -16,40 +24,46 @@ REPORT_DIR_NAMES = [
     "normal_scenario_1.25_0.7",
 ]
 
+def generate_result_str(kind, script_name, report_dir_names, report_file_count):
+    result_str = f"{kind}_{script_name}:\n"
 
-result_str = f"{KIND}_{SCRIPT_NAME}:\n"
+    for report_dir_name in report_dir_names:
+        parent_dir_name, child_dir_name = script_name.rsplit("_", 1)
+        result_dir_name = f"./results/{parent_dir_name}/{child_dir_name}/{report_dir_name}"
 
-for report_dir_name in REPORT_DIR_NAMES:
-    parent_dir_name, child_dir_name = SCRIPT_NAME.rsplit("_", 1)
-    result_dir_name = f"./results/{parent_dir_name}/{child_dir_name}/{report_dir_name}"
+        history_paths = r""""""
+        for it in range(1, report_file_count + 1):
+            report_filename = f"{report_dir_name}_{it}.json"
+            base_history_paths = rf"        reports/ecmus/$(date_ecmus)/{report_dir_name}/{report_filename} \
+                reports/kube-schedule/$(date_kube_schedule)/{report_dir_name}/{report_filename} \
+                reports/ecmus-no-migration/$(date_ecmus_no_migration)/{report_dir_name}/{report_filename} \
+                reports/random-scheduler/$(date_random)/{report_dir_name}/{report_filename} \
+                reports/cloud-first-scheduler/$(date_cloud_first)/{report_dir_name}/{report_filename} \
+                reports/smallest-edge-first-scheduler/$(date_smallest_edge_first)/{report_dir_name}/{report_filename} \
+                reports/biggest-edge-first-scheduler/$(date_biggest_edge_first)/{report_dir_name}/{report_filename} \
+                reports/ecmus-qos-aware/$(date_ecmus_qos_aware)/{report_dir_name}/{report_filename} " + "\\"
 
-    history_paths = r""""""
-    for it in range(1, REPORT_FILE_COUNT + 1):
-        report_filename = f"{report_dir_name}_{it}.json"
-        base_history_paths = rf"        reports/ecmus/$(date_ecmus)/{report_dir_name}/{report_filename} \
-            reports/kube-schedule/$(date_kube_schedule)/{report_dir_name}/{report_filename} \
-            reports/ecmus-no-migration/$(date_ecmus_no_migration)/{report_dir_name}/{report_filename} \
-            reports/random-scheduler/$(date_random)/{report_dir_name}/{report_filename} \
-            reports/cloud-first-scheduler/$(date_cloud_first)/{report_dir_name}/{report_filename} \
-            reports/smallest-edge-first-scheduler/$(date_smallest_edge_first)/{report_dir_name}/{report_filename} \
-            reports/biggest-edge-first-scheduler/$(date_biggest_edge_first)/{report_dir_name}/{report_filename} " + "\\"
+            if it != report_file_count:
+                base_history_paths += "\n"
 
-        if it != REPORT_FILE_COUNT:
-            base_history_paths += "\n"
+            history_paths += base_history_paths
 
-        history_paths += base_history_paths
+        command_str = "\t python3 main.py \\\n" + rf"""    --script_name {script_name} \
+            --config_path config.json \
+            --history_paths \
+        {history_paths}
+            --scenario-name {report_dir_name} \
+            --save-path {result_dir_name}
+        """
 
-    command_str = "\t python3 main.py \\\n" + rf"""    --script_name {SCRIPT_NAME} \
-        --config_path config.json \
-        --history_paths \
-    {history_paths}
-        --scenario-name {report_dir_name} \
-        --save-path {result_dir_name}
-    """
+        command_str += "\n"
+        result_str += command_str
 
-    command_str += "\n"
-    result_str += command_str
+    return result_str
+
 
 if __name__ == "__main__":
     with open(FILE_PATH, "a") as file:
-        file.write(result_str)
+        for script_name in SCRIPT_NAMES:
+            result_str = generate_result_str(KIND, script_name, REPORT_DIR_NAMES, REPORT_FILE_COUNT) + "\n"
+            file.write(result_str)
