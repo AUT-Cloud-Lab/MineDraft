4666c005440899338cea27b37585025e86b48987
amsen <ahph1380@gmail.com>
2025-01-03 18:55:34 -0500

    Temp fix for schedulers bug

--- extractors/logic.py
+++ extractors/logic.py
@@ -13,6 +13,9 @@ from historical.data import Cycle, ScenarioData
 
 T = TypeVar("T")
 
+# ! WARN THIS PART IS JUST A QUICK FIX TO EXECUTION PROBLEM
+# ! WARN HAVE TO REMOVE THIS PART AFTER FIXING THE EXECUTION PROBLEM
+BUGGED_SCHEDULERS = ["smallest-edge-first-scheduler", "biggest-edge-first-scheduler", "random-scheduler", "cloud-first-scheduler"]
 
 def calc_through_time(
     config: Config,
@@ -34,6 +37,13 @@ def calc_through_time(
 
     for deployment in config.deployments.values():
         for sched in schedulers:
+            if sched.name in BUGGED_SCHEDULERS:
+                # ! WARN THIS PART IS JUST A QUICK FIX TO EXECUTION PROBLEM
+                # ! WARN HAVE TO REMOVE THIS PART AFTER FIXING THE EXECUTION PROBLEM
+                for node in config.nodes.values():
+                    node.resources[0] += 1
+                    node.resources[1] += 1
+
             current_results_lists = []
             current_timestamps_lists = []
             for history in scenario.scheduler_histories[sched]:
@@ -48,6 +58,13 @@ def calc_through_time(
             timestamps[sched][deployment] = merge_lists_by_average(
                 *current_timestamps_lists
             )
+
+            if sched.name in BUGGED_SCHEDULERS:
+                # ! WARN THIS PART IS JUST A QUICK FIX TO EXECUTION PROBLEM
+                # ! WARN HAVE TO REMOVE THIS PART AFTER FIXING THE EXECUTION PROBLEM
+                for node in config.nodes.values():
+                    node.resources[0] -= 1
+                    node.resources[1] -= 1
 
     return results, timestamps
 
@@ -81,18 +98,17 @@ def calc_edge_utilization_through_time(
     Dict[Scheduler, List[float]],
     Dict[Scheduler, List[float]],
 ]:
-    edge_total_cpu, edge_total_memory = config.get_edge_resources()
-
     def edge_utilization_on_each_cycle(deployment: Deployment, cycle: Cycle) -> float:
+        edge_total_cpu, edge_total_memory = config.get_edge_resources()
         edge_usage_sum_cpu, edge_usage_sum_memory = calculate_edge_usage_sum(cycle)
 
-        if edge_total_cpu < edge_usage_sum_cpu + 1e-9:
+        if edge_total_cpu + 0.1 < edge_usage_sum_cpu:
             print(
                 f"WARNING: edge_total_cpu({edge_total_cpu}) < edge_usage_sum_cpu({edge_usage_sum_cpu}) as in cycle({cycle.timestamp}) of scenario({scenario.name})"
             )
             edge_usage_sum_cpu = edge_total_cpu
 
-        if edge_total_memory < edge_usage_sum_memory + 1e-9:
+        if edge_total_memory + 0.1 < edge_usage_sum_memory:
             print(
                 f"WARNING: edge_total_memory({edge_total_memory}) < edge_usage_sum_memory({edge_usage_sum_memory}) as in cycle({cycle.timestamp}) of scenario({scenario.name})"
             )
